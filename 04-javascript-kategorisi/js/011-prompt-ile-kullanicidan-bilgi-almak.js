f43b82aed506c6858b392c962b654b4b63bdb285
hasankokes <hasankokes@protonmail.com>
2022-04-10 14:05:21 +0300

    toplu

--- 04-javascript-kategorisi/js/011-prompt-ile-kullanicidan-bilgi-almak.js
+++ 04-javascript-kategorisi/js/011-prompt-ile-kullanicidan-bilgi-almak.js
@@ -2,7 +2,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
 
 let fullName = prompt("Lutfen Adinizi Giriniz: ")
-
+// id olduğu için başına diyez konur.!!
 let greeting = document.querySelector("#greeting")
 
 greeting.innerHTML = `${greeting.innerHTML} <small style="color:red">${fullName}</small>`
